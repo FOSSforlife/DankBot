ac49e225734ff389a496950b372dab9f75fc8187
Elias Jackson <ejthedj0@gmail.com>
2019-11-09 20:41:05 -0800

    Added now playing command

---
+++ plugins/music/commands/np.js
@@ -0,0 +1,58 @@
+const request = require('request');
+const Discord = require(`discord.js`);
+
+const radioServer = {
+  voiceChannel: 'EJ the DJ 📻',
+  jsonUrl: 'http://stream.eliasjackson.com/status-json.xsl',
+  streamUrl: 'http://stream.eliasjackson.com/music1.ogg'
+}
+
+exports.run = async(bot, message, args, level) => {
+  request(radioServer.jsonUrl, (error, response, body) => {
+    if(error || parseInt(response.statusCode) >= 400) return message.channel.send("Error getting stream data");
+
+    let icestats = JSON.parse(body).icestats;
+    let server;
+    if(Array.isArray(icestats.source)) {
+      server = icestats.source.find(s => s.server_name === 'music1.ogg');
+    }
+    else {
+      server = icestats.source;
+    }
+
+    var embed = new Discord.RichEmbed();
+    embed.setColor(bot.embedColor.info);
+    embed.setTitle(`Now playing in ${radioServer.voiceChannel}`);
+    embed.setDescription(`${server.artist} - ${server.title}`);
+    embed.setFooter(`${message.botDisplayName} v${bot.version}`);
+    embed.setTimestamp();
+    message.channel.send({
+        embed: embed
+    });
+  });
+}
+
+exports.conf = {
+  // Basic Info
+  enabled: true,
+  guildOnly: false,
+  nsfw: false,
+
+  // Command Information
+  command_name: "np",
+  command_description: "Get the currently playing track in the 24/7 audio channel.",
+  command_usage: "np",
+
+  // Alliases
+  aliases: ['nowplaying'],
+
+  // User Permissions
+  user_permissions: "User",
+
+  // Guild Permissions
+  guild_permissions: [],
+
+  // Stats
+  userStats: true,
+  guildStats: true
+}
